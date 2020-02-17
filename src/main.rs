76e40630c69c501505296c896fbf8ce74c8ecb8e
nvenetucci <47019781+nvenetucci@users.noreply.github.com>
2020-02-17 13:10:58 -0800

    Added regex parsing for date

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use regex::Regex;
 use std::io;
 use std::io::Write;
 
 fn main() {
+    let date_re =
+        Regex::new(r"^(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\d\d$").unwrap();
+
     loop {
         println!("\n1) Add an appointment");
         println!("2) Search for appointments");
@@ -19,7 +23,7 @@ fn main() {
         let input_option: u32 = match input_option.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Invalid option");
+                println!("Invalid option. Must be a valid number");
                 continue;
             }
         };
@@ -33,7 +37,34 @@ fn main() {
                 .read_line(&mut owner)
                 .expect("Failed to read line");
 
-            println!("You entered {}", owner.trim());
+            let mut description = String::new();
+
+            print!("Enter a description: ");
+            io::stdout().flush().unwrap();
+            io::stdin()
+                .read_line(&mut description)
+                .expect("Failed to read line");
+
+            let mut start_date = String::new();
+
+            loop {
+                print!("Enter the start date: ");
+                io::stdout().flush().unwrap();
+                io::stdin()
+                    .read_line(&mut start_date)
+                    .expect("Failed to read line");
+
+                if date_re.is_match(&start_date.trim()) {
+                    break;
+                } else {
+                    println!("Invalid date. Required format: mm/dd/yyyy");
+                    start_date = "".to_string();
+                }
+            }
+
+            println!("Owner: {}", owner.trim());
+            println!("Description: {}", description.trim());
+            println!("Start date: {}", start_date.trim());
         } else if input_option == 2 {
             println!("You entered {}", input_option);
             println!("This functionality isn't implemented yet");
